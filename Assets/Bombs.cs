a3aca7fbc83c1dd1c0940b4b0e64540b928f1bae
Vivek Raman <vr.ac4bf@live.com>
2019-07-11 10:59:57 +0530

    Initial commit

---
+++ Assets/Bombs.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bombs : MonoBehaviour
+{
+    public Rigidbody HipReference = null;
+
+    [Header("Explosion Parameters")]
+    [SerializeField] private float Magnitude = 100f;
+    [SerializeField] private float Radius = 50f;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ViewportPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 100f, 8))
+        {
+            HipReference.AddExplosionForce(Magnitude, hit.point, Radius);
+        }
+    }
+}
